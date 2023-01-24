59886172fdeb8bf2013d6d98d237d7d4613ef4f8
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-01-24 09:20:57 +0530

    create register and login with asinc and await

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,29 +7,74 @@ router.get("/", (req, res) => {
   res.send("server side running router js");
 });
 
-router.post("/register", (req, res) => {
+//promise approach
+
+// router.post("/register", (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(400).json({ message: "Please fill all fields" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       console.log(userExist);
+
+//       if (userExist) {
+//         return res.status(400).json({ message: "Email already exists" });
+//       }
+//       const user = new User({
+//         name, email, phone, work, password, cpassword
+//       })
+//       user.save().then(() => {
+//         return res.status(403).json({ message: "access granted" });
+//       }).catch((err) => {
+//         res.status(500).json({ message: err.message });
+
+//       })
+//     }).catch((err) => { console.log(err) })
+// });
+
+//async approach
+
+router.post("/register", async (req, res) => {
+
   const { name, email, phone, work, password, cpassword } = req.body;
+
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(400).json({ message: "Please fill all fields" });
   }
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      console.log(userExist);
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const user = new User({
+      name, email, phone, work, password, cpassword
+    })
+    user.save()
+    return res.status(201).json({ message: "access granted" });
 
-      if (userExist) {
-        return res.status(400).json({ message: "Email already exists" });
-      }
-      const user = new User({
-        name, email, phone, work, password, cpassword
-      })
-      user.save().then(() => {
-        return res.status(403).json({ message: "access granted" });
-      }).catch((err) => {
-        res.status(500).json({ message: err.message });
-
-      })
-
-    }).catch((err) => { console.log(err) })
+  } catch (err) {
+    console.log(err.message)
+  }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) { return res.status(400).json({ message: "Please fill all fields" }); }
+    const userExist = await User.findOne({ email: email });
+    if (!userExist) {
+      res.status(400).json({ message: "login error" })
+    } else {
+      res.json({ message: "login success" })
+    }
+
+  } catch (error) {
+    console.log(error.message)
+  }
+
+});
+
 module.exports = router;
